ff0d42adfd13b6f88827f2e81b8594725351730f
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-09-04 18:08:04 +0530

    created the Class Movie and declared the attributes. created getter and setter for all the above attributes and parametirized constructor.

--- src/main/java/com/jap/Movie.java
+++ src/main/java/com/jap/Movie.java
@@ -42,5 +42,10 @@ public class Movie {
     }
 
     //complete the parameterized constructor
-
+    public Movie(int movieId, String movieName, String genre, String releaseDate) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+    }
 }
