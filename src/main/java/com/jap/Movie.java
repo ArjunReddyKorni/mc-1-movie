60744083786cdbdfb83afaf3187ec244df165dd8
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-09-04 18:05:44 +0530

    created the Class Movie and declared the attributes.

--- src/main/java/com/jap/Movie.java
+++ src/main/java/com/jap/Movie.java
@@ -1,5 +1,13 @@
 package com.jap;
 
 public class Movie {
+    private int movieId;
+    private String movieName;
+    private String genre;
+    private String releaseDate;
+
+    // create getter and setter for all the above attributes
+
+
 
 }
