4bd80120d027da8f8f4263d63c06e14c115fdf60
Korni Arjun Reddy <arjunreddykorni@gmail.com>
2022-09-04 18:12:58 +0530

    created the class MovieService. Created five movie object by calling Movie class parameterized constructor,HashMap object and added all the Movie object inside it.

--- src/main/java/com/jap/MovieService.java
+++ src/main/java/com/jap/MovieService.java
@@ -4,5 +4,30 @@ import java.util.*;
 
 public class MovieService {
 
+    public Map<Movie,Integer> getMovieWithRating(){
+
+        //create five movie object by calling Movie class parameterized constructor
+        Movie movie1 = new Movie(101,"A Beautiful Mind","economic","23/1/2003");
+        Movie movie2 = new Movie(102,"Two Beautiful Mind","comedy","13/1/2007");
+        Movie movie3 = new Movie(103,"Three Beautiful Mind","Romantic","11/1/2005");
+        Movie movie4 = new Movie(104,"Four Beautiful Mind","Horror","29/1/2004");
+        Movie movie5 = new Movie(105,"Five Beautiful Mind","Drama","31/1/2004");
+
+        //create HashMap object and add all the Movie object inside it
+        Map<Movie,Integer> movieMap = new HashMap();
+
+        //Key will be movie object and value will be rating ranging from 1 to 5
+        movieMap.put(movie1,3);
+        movieMap.put(movie2,4);
+        movieMap.put(movie3,5);
+        movieMap.put(movie4,4);
+        movieMap.put(movie5,3);
+
+        //return the HashMap object
+
+
+        return movieMap;
+    }
+
 
 }
